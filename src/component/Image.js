988036ab98b4501d71426ca426ccc2d284037405
Rahul singh <rahulsingh19390@gmail.com>
2023-01-06 01:46:44 +0530

    Started Working on Shine

--- src/component/Image.js
+++ src/component/Image.js
@@ -5,5 +5,16 @@ const Shine = ({imgUrl}) => {
     const shineRef = useRef(null);
 
     useEffect(() => {
-        
+        let xP=0;
+        const animate = () => {
+            shineRef.current.style.backgroundPosition = `${xP}px 0px`;
+            xP += 1;
+            requestAnimationFrame(animate);
+        };
+        animate();
+    },[]);
+
+    return (
+        <img className="shine" ref={shineRef} src={imgUrl} ></img>
+    )
 }
