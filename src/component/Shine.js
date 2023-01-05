92cbe2fe7e69e0f6032b4a4aac14150e25b0a1d7
Rahul singh <rahulsingh19390@gmail.com>
2023-01-06 01:50:44 +0530

    Some Error Come

---
+++ src/component/Shine.js
@@ -0,0 +1,21 @@
+import React ,{useEffect, useRef} from 'react';
+
+const Shine = ({imgUrl}) => {
+
+    const shineRef = useRef(null);
+
+    useEffect(() => {
+        let xP=0;
+        const animate = () => {
+            shineRef.current.style.backgroundPosition = `${xP}px 0px`;
+            xP += 1;
+            requestAnimationFrame(animate);
+        };
+        animate();
+    },[]);
+
+    return (
+        <img className="shine" ref={shineRef} src={imgUrl} ></img>
+    )
+};
+export default Shine;
